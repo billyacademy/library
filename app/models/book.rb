c5d6f2466b8e0627601a37445201cdd99728496c
William Mahoney <wm.j.mahoney@gmail.com>
2014-12-11 13:43:40 -0500

    Add associations for Categorization and Category

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,7 @@
 class Book < ActiveRecord::Base
   has_many :checkouts
-
+  has_many :categorizations
+  has_many :categories, through: :categorizations
   # def checkouts
   #   #goes to checkouts table and retrieves all records with
   #   #this book's id as it's book_id
