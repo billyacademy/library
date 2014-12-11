894e040fde0676cf2ea61c5cec48cf30276db0d6
William Mahoney <wm.j.mahoney@gmail.com>
2014-12-10 20:05:20 -0500

    Added favorite column to books table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141211003934) do
+ActiveRecord::Schema.define(version: 20141211005349) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20141211003934) do
     t.text    "description"
     t.integer "quantity",    default: 1, null: false
     t.string  "author"
+    t.boolean "favorite"
   end
 
   add_index "books", ["call_number"], name: "index_books_on_call_number", unique: true, using: :btree
